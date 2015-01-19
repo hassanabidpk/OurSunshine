3691bdcefd24af8a22bd1316258bc409ef3a410b
Hassan Abid <hassanabidpk89@gmail.com>
2015-01-19 09:49:39 +0900

    added today's view in ForeCastAdapter

--- app/src/main/java/com/mooccircle/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/mooccircle/sunshine/app/ForecastAdapter.java
@@ -16,13 +16,30 @@ import com.mooccircle.sunshine.app.util.Utility;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private static final int VIEW_TYPE_COUNT = 2;
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        if(viewType == VIEW_TYPE_TODAY)  {
+            
+            layoutId = R.layout.list_item_forecast_today;
+        } else if(viewType == VIEW_TYPE_FUTURE_DAY ) {
+
+            layoutId = R.layout.list_item_forecast;
+
+        }
+        
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     @Override
@@ -58,5 +75,15 @@ public class ForecastAdapter extends CursorAdapter {
         double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
         TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
         lowView.setText(Utility.formatTemperature(low, isMetric));
+    }
+
+    @Override
+    public  int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0)? VIEW_TYPE_TODAY: VIEW_TYPE_FUTURE_DAY;
     }
 }
